5c9f539e0c81d71d7d7b427342b352beacb1543f
Javed Chauhan <chauhanjaved@gmail.com>
2023-09-27 18:26:46 +0530

    Update in style

--- src/App.js
+++ src/App.js
@@ -6,35 +6,61 @@ function App() {
   const [sessionLength, setSessionLength] = useState(25);
   return (
     <Fragment>
-      <h1 className="text-center bg-dark text-white p-3">25 + 5 Clock</h1>
-      <div id="main" className="container">              
-        <div id="length-btn-box" className="d-flex justify-content-evenly align-items-center">
+      <h1 className='text-center bg-dark text-white p-3'>25 + 5 Clock</h1>
+      <div id='main' className='container'>
+        <div id='length-btn-box' className='d-flex justify-content-center' >
           <div>
-            <h4 id="break-label" className="text-center">Break<br/>Length</h4>            
-            <div className="d-flex justify-content-center">
-              <button id="break-decrement" className="btn p-1"><i class="bi-arrow-down-circle-fill display-6"></i></button>
-              <div id="break-length" className="display-6 p-1">{breakLength}</div>
-              <button id="break-increment" className="btn p-1"><i class="bi-arrow-up-circle-fill display-6"></i></button>              
+            <h4 id='break-label' className='text-center'>
+              Break Length
+            </h4>
+            <div className='d-flex justify-content-center'>
+              <button id='break-decrement' className='btn btn-dark m-3 text-center rounded-circle'>
+                <i className='bi-arrow-down-circle-fill'></i>
+              </button>
+              <div id='break-length' className='m-3'>
+                {breakLength}
+              </div>
+              <button id='break-increment' className='btn btn-dark m-3 text-center rounded-circle'>
+                <i className='bi-arrow-up-circle-fill'></i>
+              </button>
             </div>
           </div>
           <div>
-          <h4 id="session-label" className="text-center">Session<br/>Length</h4>            
-            <div className="d-flex justify-content-center">
-              <button id="session-decrement" className="btn p-1"><i class="bi-arrow-down-circle-fill display-6"></i></button>
-              <div id="session-length" className="display-6 p-1">{sessionLength}</div>
-              <button id="session-increment" className="btn p-1"><i class="bi-arrow-up-circle-fill display-6"></i></button>              
+            <h4 id='session-label' className='text-center'>
+              Session Length
+            </h4>
+            <div className='d-flex justify-content-center'>
+              <button id='session-decrement' className='btn btn-dark m-3 text-center rounded-circle fs-4'>
+                <i className='bi-arrow-down-circle-fill'></i>
+              </button>
+              <div id='session-length' className='m-3'>
+                {sessionLength}
+              </div>
+              <button id='session-increment' className='btn btn-dark m-3 text-center rounded-circle'>
+                <i className='bi-arrow-up-circle-fill'></i>
+              </button>
             </div>
           </div>
         </div>
-        <div id="session-timer-box" className="d-flex flex-column justify-content-center mt-3">
-          <h2 id="timer-label" className="text-center">Session</h2>
-          <div id="time-left"  className="text-center">25:00</div>
+        <div id='session-timer-box' className='d-flex flex-column justify-content-center mt-3'>
+          <h2 id='timer-label' className='text-center'>
+            Session
+          </h2>
+          <div id='time-left' className='text-center'>
+            25:00
+          </div>
         </div>
-        <div id="action-tbn-box" className="d-flex justify-content-center">
-              <button id="start_stop" className="btn p-3"><i class="bi-play-circle-fill display-6"></i></button>
-              <button className="btn p-3"><i class="bi-pause-circle-fill display-6"></i></button>
-              <button id="reset" className="btn p-3"><i class="bi-reply-fill display-6"></i></button>
-          </div>        
+        <div id='action-tbn-box' className='d-flex justify-content-center align-items-center'>
+          <button id='start_stop' className='btn btn-dark m-3 text-center rounded-circle fs-3'>
+            <i className='bi bi-play-circle-fill'></i>
+          </button>
+          <button className='btn btn-dark m-3 text-center rounded-circle fs-3'>
+            <i className='bi-pause-circle-fill'></i>
+          </button>
+          <button id='reset' className='btn btn-dark m-3 text-center rounded-circle fs-3'>
+            <i className='bi bi-reply-fill'></i>
+          </button>
+        </div>
       </div>
     </Fragment>
   );
