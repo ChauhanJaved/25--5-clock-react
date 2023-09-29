d3f97613458763cdeee8b8ea5b3649d27587c5e6
Javed Chauhan <chauhanjaved@gmail.com>
2023-09-29 09:52:03 +0530

    Update logic in timmer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState, useEffect, useRef } from "react";
+import beepSound from "./beep.wav";
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
+  const [curSeconds, setCurSeconds] = useState(25 * 60);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
-  const [timerType, setTimerType] = useState("session");
+  const [timerType, setTimerType] = useState("Session");
   const [intervalID, setIntervalID] = useState(null);
-  const [curSeconds, setCurSeconds] = useState(1500);
- 
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (curSeconds === 0) {
+      audioRef.current.play();
+      if (timerType === "Session") {
+        setTimerType("Break");
+        setCurSeconds(breakLength * 60);
+      } else {
+        setTimerType("Session");
+        setCurSeconds(sessionLength * 60);
+      }
+    }
+  }, [curSeconds]);
+
+  // Set break length
   const breakLengthClick = (keyType) => {
     if (keyType === "D") {
       if (!(breakLength <= 1)) {
@@ -21,6 +37,8 @@ function App() {
       }
     }
   };
+
+  // Set session length
   const sessionLengthClick = (keyType) => {
     if (keyType === "D") {
       if (!(sessionLength <= 1)) {
@@ -32,72 +50,94 @@ function App() {
       }
     }
   };
-  const playClick = () => {
-    if (isPlaying) {      
-      clearInterval(intervalID)
-      setIsPaused(true);
-    } else {            
-      if (!isPaused) {
-        if (timerType === 'session') {
-          setCurSeconds(sessionLength * 60);
-        }else {
-          setCurSeconds(breakLength * 60);
-        }
-        setIntervalID(setInterval(timerFunction, 1000));        
-      }else {
 
-        setIntervalID(setInterval(timerFunction, 1000));        
-      };
-      
+  // Play buttion click
+  const playClick = () => {
+    if (!isPlaying && !isPaused) {
+      setIsPlaying(true);
+      setCurSeconds(sessionLength * 60);
+      setIntervalID(setInterval(timerFunction, 1000));
+    } else if (isPlaying && !isPaused) {
+      setIsPaused(true);
+      clearInterval(intervalID);
+    } else if (isPlaying && isPaused) {
+      setIsPaused(false);
+      setIntervalID(setInterval(timerFunction, 1000));
     }
   };
-  const timerFunction = () => {    
-    setCurSeconds((prevCurSeconds) => prevCurSeconds - 1);    
+
+  //Timmer function
+  const timerFunction = () => {
+    setCurSeconds((prevCurSeconds) => prevCurSeconds - 1);
   };
+
+  // Reset defaults
   const handleReset = () => {
+    clearInterval(intervalID);
+    setTimerType("Session");
     setBreakLength(5);
     setSessionLength(25);
-    setTimerType("session");
+    setCurSeconds(25 * 60);
+    setIsPlaying(false);
+    setIsPaused(false);
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+  };
+
+  // Stop timer
+  const handleStop = () => {
     clearInterval(intervalID);
-    setCurSeconds(1500);
-    setIsPlaying(false);   
+    setTimerType("Session");
+    setCurSeconds(sessionLength * 60);
+    setIsPlaying(false);
+    setIsPaused(false);
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
   };
+
+  // Format min and sec string based on curSeconds
   const formatTime = () => {
-    const minutes = Math.floor(curSeconds / 60).toString().padStart(2,'0');
-    const seconds = (curSeconds % 60).toString().padStart(2,'0');
-    return `${minutes}:${seconds}`
+    const minutes = Math.floor(curSeconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (curSeconds % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
   };
+
   return (
     <Fragment>
+      {/* Beep audio  */}
+      <audio ref={audioRef} id='beep' src={beepSound}></audio>
+
       <h1 className='text-center bg-dark text-white p-3'>25 + 5 Clock</h1>
       <div id='main' className='container'>
         {/* PLAY PAUSE BOX */}
         <div id='action-tbn-box' className='d-flex justify-content-center align-items-center fs-6 mt-2'>
           <button id='start_stop' className='btn btn-dark text-center' onClick={playClick}>
-            {!isPaused ? (
-              <i id='play' className='bi bi-play-circle-fill'></i>
-            ) : (
+            {isPlaying && !isPaused ? (
               <i id='play' className='bi bi-pause-circle-fill'></i>
+            ) : (
+              <i id='play' className='bi bi-play-circle-fill'></i>
             )}
           </button>
-          <button className='btn btn-dark text-center m-3'>
+          <button className='btn btn-dark text-center m-3' disabled={!isPlaying} onClick={handleStop}>
             <i className='bi-stop-circle-fill'></i>
           </button>
           <button id='reset' className='btn btn-dark text-center' onClick={handleReset}>
             <i className='bi bi-reply-fill'></i>
           </button>
         </div>
+
         {/* SESSION TIMER BOX */}
         <div id='session-timer-box' className='d-flex flex-column justify-content-center p-3 mt-2'>
           <h2 id='timer-label' className='text-center'>
             {timerType}
           </h2>
           <div id='time-left' className='text-center display-1'>
-            <strong>
-              {formatTime()}
-            </strong>
+            <strong>{formatTime()}</strong>
           </div>
         </div>
+
         {/* LENGTH SETTING BOX */}
         <div id='length-btn-box' className='d-flex justify-content-center flex-wrap mt-3 fs-6'>
           <div className='m-3'>
